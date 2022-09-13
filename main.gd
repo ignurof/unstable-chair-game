154efcab4804c5962c272fdfb7fbd8a587c66fac
Ignurof <ignurof@ignurof.com>
2022-09-13 13:19:23 +0200

    demo_level with shadered chair_proto

--- main.gd
+++ main.gd
@@ -2,7 +2,14 @@ extends Node
 # Main entrypoint to the program (game)
 
 onready var main_menu: Control = load("res://menus/main_menu.tscn").instance()
+onready var demo_level: Node2D = load("res://levels/demo_level.tscn").instance()
 
 
 func _ready() -> void:
-    add_child(main_menu.instance())
+    main_menu.connect("play", self, "_on_play")
+    add_child(main_menu)
+    
+    
+func _on_play() -> void:
+    get_child(0).queue_free()
+    add_child(demo_level)
