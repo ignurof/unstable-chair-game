c4ba0b7a6bb74021a3e4b9eed0ac2bdf397c81b8
Ignurof <ignurof@ignurof.com>
2022-09-15 22:52:39 +0200

    general stuff & word typing setup

---
+++ chair/chair.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+onready var _sprite: Sprite = $Sprite
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass
+#    rotate_chair()
+
+
+func rotate_chair() -> void:
+    pass
+#    _sprite.rotate(PI/2)
+#    var tween: Tween = Tween.new()
+#    tween.interpolate_property(_sprite, "rotation_degrees", 64, Tween.EASE_OUT, 1.0)
---
+++ levels/user_interface.gd
@@ -0,0 +1,70 @@
+extends Control
+
+var word_letter: PackedScene = load("res://fonts/word_letter.tscn")
+var type_letter: PackedScene = load("res://fonts/type_letter.tscn")
+
+var active_word: String = ""
+var current_letter
+var letter_index: int = 0
+
+onready var word_container = $WordContainer
+onready var typing_container = $TypingContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    randomize()
+    active_word = _randomly_select_word()
+    current_letter = active_word[letter_index]
+    
+    for letter in active_word:
+        var new_letter: Label = word_letter.instance()
+        new_letter.text = letter
+        word_container.add_child(new_letter)
+
+
+func _input(event) -> void:
+    if event is InputEventKey and event.pressed:
+        var scancode_string = OS.get_scancode_string(event.scancode).to_lower()
+        # Validate input key is same as current_letter in the active_word (dont let player mistype)
+        if scancode_string != current_letter:
+            print("Wrong letter!")
+            return
+            
+        var new_letter: Label = type_letter.instance()
+        new_letter.text = scancode_string
+        typing_container.add_child(new_letter)
+        printt(letter_index, current_letter, active_word.length())
+        letter_index = letter_index + 1
+        if letter_index < active_word.length():
+            current_letter = active_word[letter_index]
+        else:
+            current_letter = null    
+            
+
+func _randomly_select_word() -> String:
+    var random_number: int = randi() % 11
+    match random_number:
+        1:
+            return "hello"
+        2:
+            return "world"
+        3:
+            return "boss"
+        4:
+            return "tree"
+        5:
+            return "food"
+        6:
+            return "banana"
+        7:
+            return "alchemy"
+        8:
+            return "worldstar"
+        9:
+            return "programmer"
+        10:
+            return "developer"
+        _:
+            push_error("_randomly_select_word: Unhandled random_number case")
+            return ""
