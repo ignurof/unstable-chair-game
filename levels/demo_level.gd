e5d76c65dfe3b88b4733cdaca788559b989e0756
Ignurof <ignurof@ignurof.com>
2022-09-17 17:56:07 +0200

    word_list

--- levels/demo_level.gd
+++ levels/demo_level.gd
@@ -8,47 +8,23 @@ onready var ui: Control = $UI
 
 
 func _ready():
+    randomize()
     ui.connect("set_new_word", self, "_on_set_new_word")
-    chair.rotate_chair()
-    word_list = LoadWords.load_to_memory().split("\n")
     
+    word_list = LoadWords.load_to_memory().split("\n")
     _start()
 
 
 func _start() -> void:
     _on_set_new_word()
     ui.stability_timer.start(30.0)
+    chair.rotate_chair()
 
 
 func _randomly_select_word() -> String:
-    var random_number: int = randi() % 11
-    var new_word: String
-    match random_number:
-        0:
-            new_word = "ignurof"
-        1:
-            new_word = "hello"
-        2:
-            new_word = "world"
-        3:
-            new_word = "boss"
-        4:
-            new_word = "tree"
-        5:
-            new_word = "food"
-        6:
-            new_word = "banana"
-        7:
-            new_word = "alchemy"
-        8:
-            new_word = "worldstar"
-        9:
-            new_word = "programmer"
-        10:
-            new_word = "developer"
-        _:
-            push_error("_randomly_select_word: Unhandled random_number case")
-            return ""
+    var new_word = "ignurof"
+    var rand_num = randi() % len(word_list)
+    new_word = word_list[rand_num]
             
     # HACK: Never use the same word twice recursion
     if _old_word == new_word:
