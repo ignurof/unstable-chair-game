ad61e36638a038f4af2f705b4b98aeafe27f5410
Ignurof <ignurof@ignurof.com>
2022-09-25 14:37:38 +0200

    menu audio, refactor

--- menus/game_over_menu.gd
+++ menus/game_over_menu.gd
@@ -3,12 +3,19 @@ extends Control
 signal play
 signal back_to_main
 
+onready var play_audio = $PlayAudio
+onready var quit_audio = $QuitAudio
+
 
 func _on_Play_pressed() -> void:
+    play_audio.play()
+    yield(play_audio, "finished")
     emit_signal("play")
 
 
 func _on_Quit_pressed() -> void:
+    quit_audio.play()
+    yield(quit_audio, "finished")
     emit_signal("back_to_main")
 
 
--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -2,16 +2,17 @@ extends Control
 
 signal play
 
-onready var options_menu: Control = $OptionsMenu
+onready var play_audio = $PlayAudio
+onready var quit_audio = $QuitAudio
 
 
 func _on_Play_pressed():
+    play_audio.play()
+    yield(play_audio, "finished")
     emit_signal("play")
 
 
-func _on_Options_pressed():
-    options_menu.set_visible(!options_menu.visible)
-
-
 func _on_Quit_pressed():
+    quit_audio.play()
+    yield(quit_audio, "finished")
     get_tree().quit()
